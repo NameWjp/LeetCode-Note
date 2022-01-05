0e66c3b08306a2cfc9a7f9968f87681165d89a06
wangjp <943375372@qq.com>
2022-01-05 11:31:28 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+## LeetCode 笔记 
+在线链接：[LeetCode 笔记](https://namewjp.github.io/LeetCode-Note/)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    base: '/Front-end-interview/',
+    title: '前端面试题',
+    head: [
+        ['meta', { name: 'referrer', content: 'no-referrer' }],
+    ],
+    themeConfig: {
+        sidebar: [
+            ['/', '简介'],
+            ['/数组', '数组'],
+        ],
+        displayAllHeaders: true,
+        smoothScroll: true,
+    }
+}
