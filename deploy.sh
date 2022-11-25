b32475d5fdac14f5a603742b87b9a2d55255f468
王金平 <32758131+NameWjp@users.noreply.github.com>
2022-11-25 23:07:49 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:NameWjp/LeetCode-Note.git main:gh-pages
+git push -f git@github.com:NameWjp/LeetCode-Note.git master:gh-pages
 
 cd -
